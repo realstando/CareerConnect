4ae8412675b9e689cfed26108789a1162ea55b5c
Ryan Wang <rryanwwang@gmail.com>
2024-11-14 20:48:25 -0800

    configured firebase and finished postings submissions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,7 +12,17 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { firebaseConfig } from '../firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"careerconnect-a5e78","appId":"1:1064873679635:web:604bfd6ddc307de1a5d8e6","storageBucket":"careerconnect-a5e78.firebasestorage.app","apiKey":"AIzaSyDDj-kceIH1c5xQYcjV5nAucgA2uzlyX78","authDomain":"careerconnect-a5e78.firebaseapp.com","messagingSenderId":"1064873679635","measurementId":"G-N679B8T2JJ"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ],
 };
--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  CollectionReference,
+  DocumentData,
+  Firestore,
+  addDoc,
+  collection,
+} from '@angular/fire/firestore';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-submit',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDialogModule,
+  ],
   templateUrl: './submit.component.html',
-  styleUrl: './submit.component.scss'
+  styleUrl: './submit.component.scss',
 })
-export class SubmitComponent {
+export class SubmitComponent implements OnInit {
+  form!: FormGroup;
 
-}
+  constructor(private firestore: Firestore) {}
+
+  allPostings!: CollectionReference<DocumentData, DocumentData>;
+
+  ngOnInit(): void {
+    this.allPostings = collection(this.firestore, 'all-job-postings');
+  }
+
+  private fb = inject(FormBuilder);
+  posting = this.fb.group({
+    company: [null, Validators.required],
+    companyCont: [null, Validators.required],
+    companyDesc: [null, Validators.required],
+    job: [null, Validators.required],
+    jobResp: [null, Validators.required],
+    jobQual: [null, Validators.required],
+    jobLoc: [null, Validators.required],
+    jobSal: [null, Validators.required],
+  });
+
+  onSubmit(): void {
+    addDoc(this.allPostings, this.posting.value);
+    this.posting.reset();
+  }
+} 
